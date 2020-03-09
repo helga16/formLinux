071f13b68340eb1c084b1a919f4e8d0e5317152f
Helga <melamori1605@gmail.com>
2020-03-09 23:09:43 +0200

    changes werw added

--- index.php
+++ index.php
@@ -76,11 +76,14 @@
 </form>
 </div>
 
+
+</body>
+</html>
 <?php
 if(isset($_POST['email']) and isset($_POST['login'])){
   try{
-	$email=$_POST['email'];
-	$login = $_POST['login'];
+  $email=$_POST['email'];
+  $login = $_POST['login'];
   $host='localhost';
 $dbName = 'test';
 $pass = '';
@@ -90,8 +93,8 @@ $charset = 'utf8';
 $dsn = "mysql:host=$host;dbname=$dbName;charset=$charset";
 $pdo = new PDO($dsn, $login,$pass);
 
-	$stmt=$pdo->prepare('INSERT INTO people (login,email, address,Full_name, city, state, code) values(?, ?, ?,?,?,?,?)');
-	$stmt->execute(array($login, $email, $_POST['Address'], $_POST['Full_name'],$_POST['city'],$_POST['state'],$_POST['code']));
+  $stmt=$pdo->prepare('INSERT INTO people (login,email, address,Full_name, city, state, code) values(?, ?, ?,?,?,?,?)');
+  $stmt->execute(array($login, $email, $_POST['Address'], $_POST['Full_name'],$_POST['city'],$_POST['state'],$_POST['code']));
 }
  catch (PDOException $e) {
   exit ('Error connecting to database: ' . $e->getMessage());
@@ -99,5 +102,3 @@ $pdo = new PDO($dsn, $login,$pass);
 }
 
 ?>
-</body>
-</html>
